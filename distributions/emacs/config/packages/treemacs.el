7f8a4f79ae90641c25398a7fa25ef2edb93035dc
redjax <5534031+redjax@users.noreply.github.com>
2025-05-19 02:04:07 -0400

    Add configs to treemacs

--- distributions/emacs/config/packages/treemacs.el
+++ distributions/emacs/config/packages/treemacs.el
@@ -5,4 +5,12 @@
   :bind
   (("C-x t t" . treemacs))  ;; You can pick any keybinding you like
   :config
-  (setq treemacs-width 35)) ;; Optional: set the width of the Treemacs window
+  (setq treemacs-width 35)
+  (treemacs-follow-mode 1)
+  (treemacs-filewatch-mode 1)
+  (treemacs-git-mode 'deferred)) ;; Optional: set the width of the Treemacs window
+
+(use-package treemacs-nerd-icons
+  :ensure t
+  :config
+  (treemacs-load-theme "nerd-icons"))
