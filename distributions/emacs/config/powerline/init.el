c998ea50703b7f0e422ad35e9abee912c8c64fb9
redjax <5534031+redjax@users.noreply.github.com>
2025-05-20 01:33:57 -0400

    Disable moody

--- distributions/emacs/config/powerline/init.el
+++ distributions/emacs/config/powerline/init.el
@@ -1,8 +1,8 @@
 ;; Load powerline
 
 ;; Moody
-(load (expand-file-name "moody.el" (file-name-directory load-file-name)))
-(load (expand-file-name "minions.el" (file-name-directory load-file-name)))
+;; (load (expand-file-name "moody.el" (file-name-directory load-file-name)))
+;; (load (expand-file-name "minions.el" (file-name-directory load-file-name)))
 
 ;; Smart-mode-line
 (load (expand-file-name "smart-mode-line.el" (file-name-directory load-file-name)))
