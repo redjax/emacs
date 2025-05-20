ac0c0bda23fd5d4cfe482c55d10e9c092fa0612b
redjax <5534031+redjax@users.noreply.github.com>
2025-05-19 22:50:03 -0400

    Add zenburn.
    Add comments to init.el

--- distributions/emacs/config/theme/init.el
+++ distributions/emacs/config/theme/init.el
@@ -1,13 +1,28 @@
 ;; Theme loader -- only activate one at a time!
 
 ;; Uncomment the theme you want to use, comment out the rest.
+;  Activate a theme in the "Activate theme theme" section below
 
+;; Catppuccin
 ;; (load (expand-file-name "catppuccin-theme.el" (file-name-directory load-file-name)))
+
+;; Nordic night
 (load (expand-file-name "nordic-night-theme.el" (file-name-directory load-file-name)))
+
+;; Zeno
 ;; (load (expand-file-name "zeno-theme.el" (file-name-directory load-file-name)))
+
+;; Melancholy
 ;; (load (expand-file-name "melancholy-theme.el" (file-name-directory load-file-name)))
+
+;; Doom
 ;; (load (expand-file-name "doom-themes.el" (file-name-directory load-file-name)))
+
+;; Modus themes
 ;; (load (expand-file-name "modus-themes.el" (file-name-directory load-file-name)))
+
+;; Zenburn
+;; (load (expand-file-name "zenburn-theme.el" (file-name-directory load-file-name)))
 
 ;; Disable any previously enabled themes for a clean switch
 (mapc #'disable-theme custom-enabled-themes)
